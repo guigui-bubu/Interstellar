bf21e1f0d7e3f9e890eaa3ac9a972a6fd18a1f69
guigui-bubu <58784086+guigui-bubu@users.noreply.github.com>
2020-02-24 17:23:56 +0100

    setting

--- App.js
+++ App.js
@@ -323,12 +323,3 @@ export default function App() {
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
